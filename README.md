aa79f13247ff1063f327f905a3e8bda3d22a4a00
Thor Nolan <tnolan5764@gmail.com>
2019-03-25 16:51:39 -0700

    Set up preliminary files and schema for mySQL database.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MySQL-Bamazon
-A CLI storefront app that uses a MySQL database to store its product information
+A CLI storefront app that uses a MySQL database on the back-end to store its product information. 
---
+++ schema.sql
@@ -0,0 +1,17 @@
+-- set-up for my Bamazon database --
+DROP DATABASE IF EXISTS Bamazon_db;
+CREATE DATABASE Bamazon_db;
+USE Bamazon_db;
+
+-- table to store my products and serve as an inventory --
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
+
+-- Populate my table with products --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
